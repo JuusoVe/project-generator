e735d37f2021a27636fd7e52a44a6218d2d48705
Juuso <juuso.vesanto@gmail.com>
2022-12-17 11:40:34 +0200

    feat: Require process envs to start server.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import { checkRequiredEnvs } from './utils'
 
+export const REQUIRED_ENVS = {
+    templateRepoOwner: 'TEMPLATE_REPO_OWNER',
+    templateRepoName: 'TEMPLATE_REPO_NAME',
+}
+
+checkRequiredEnvs(REQUIRED_ENVS, process.env)
--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,35 @@
-import libsodium from "libsodium-wrappers";
+import libsodium from 'libsodium-wrappers'
 
 export const encryptSecret = async (publicKey: string, secretValue: string) => {
-    await libsodium.ready;
+    await libsodium.ready
 
     // Convert the message and key to Uint8Array's (Buffer implements that interface)
-    const messageBytes = Buffer.from(secretValue);
-    const keyBytes = Buffer.from(publicKey, 'base64');
+    const messageBytes = Buffer.from(secretValue)
+    const keyBytes = Buffer.from(publicKey, 'base64')
 
     // Encrypt using LibSodium.
-    const encryptedBytes = libsodium.crypto_box_seal(messageBytes, keyBytes);
+    const encryptedBytes = libsodium.crypto_box_seal(messageBytes, keyBytes)
 
     // Base64 the encrypted secret
-    const encrypted = Buffer.from(encryptedBytes).toString('base64');
+    const encrypted = Buffer.from(encryptedBytes).toString('base64')
     return encrypted
+}
+
+export const checkRequiredEnvs = (
+    requiredEnvs: object,
+    processEnvs: NodeJS.ProcessEnv
+) => {
+    const missingEnvs: string[] = []
+
+    Object.values(requiredEnvs).forEach((envKey) => {
+        if (!processEnvs[envKey]) {
+            missingEnvs.push(envKey)
+        }
+    })
+
+    if (missingEnvs.length) {
+        throw new Error(
+            `Missing required environment variables: ${missingEnvs.toString()}`
+        )
+    }
 }
