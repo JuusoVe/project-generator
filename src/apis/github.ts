e27a4f13b05848a2a88617a75c808c08de617799
Juuso <juuso.vesanto@gmail.com>
2022-12-17 11:49:15 +0200

    feat: Use repo template in creating target repo.

--- src/apis/github.ts
+++ src/apis/github.ts
@@ -1,24 +1,62 @@
-import { Octokit } from "@octokit/rest";
-import { Endpoints } from "@octokit/types";
-
+import { Octokit } from '@octokit/rest'
+import { Endpoints } from '@octokit/types'
 
 const githubAPIClient = new Octokit({
     auth: process.env.GITHUB_ACCESS_KEY,
-    userAgent: 'coreGenerator'
+    userAgent: 'coreGenerator',
 })
 
-export type GithubRepo = Endpoints["POST /user/repos"]["response"]["data"]
+export type GithubRepo = Endpoints['POST /user/repos']['response']['data']
 
-const createRepo = async (repoName: string) => await githubAPIClient.rest.repos.createForAuthenticatedUser({ name: repoName })
-const deleteUserRepo = async (username: string, repoName: string) => await githubAPIClient.rest.repos.delete({ owner: username, repo: repoName })
-const getRepoPublicEncryptionKey = async (username: string, repoName: string) => await githubAPIClient.rest.actions.getRepoPublicKey({ repo: repoName, owner: username })
-const createRepoSecret = async (username: string, repoName: string, secretName: string, secretValue: string, keyId: string) => {
-    await githubAPIClient.rest.actions.createOrUpdateRepoSecret({ owner: username, repo: repoName, secret_name: secretName, encrypted_value: secretValue, key_id: keyId })
+const createRepoFromTemplate = async (
+    newRepoName: string,
+    templateRepoName: string,
+    templateRepoOwner: string
+) => {
+    return await githubAPIClient.rest.repos.createUsingTemplate({
+        name: newRepoName,
+        template_repo: templateRepoName,
+        template_owner: templateRepoOwner,
+        private: true,
+    })
+}
+
+const deleteUserRepo = async (username: string, repoName: string) => {
+    return await githubAPIClient.rest.repos.delete({
+        owner: username,
+        repo: repoName,
+    })
+}
+
+const getRepoPublicEncryptionKey = async (
+    username: string,
+    repoName: string
+) => {
+    return await githubAPIClient.rest.actions.getRepoPublicKey({
+        repo: repoName,
+        owner: username,
+    })
+}
+
+const createRepoSecret = async (
+    username: string,
+    repoName: string,
+    secretName: string,
+    secretValue: string,
+    keyId: string
+) => {
+    return await githubAPIClient.rest.actions.createOrUpdateRepoSecret({
+        owner: username,
+        repo: repoName,
+        secret_name: secretName,
+        encrypted_value: secretValue,
+        key_id: keyId,
+    })
 }
 
 export default {
-    createRepo,
+    createRepoFromTemplate,
     deleteUserRepo,
     getRepoPublicEncryptionKey,
-    createRepoSecret
+    createRepoSecret,
 }
--- src/routes/repos.ts
+++ src/routes/repos.ts
@@ -1,30 +1,50 @@
-import { Router } from "express";
+import { Router } from 'express'
+import { REQUIRED_ENVS } from '..'
 import githubAPIClient from '../apis/github'
-import { encryptSecret } from "../utils";
+import { encryptSecret } from '../utils'
 
 export const REPOS_GITHUB_PATH = '/repos/github'
 export const SECRETS_PATH = '/secrets'
+
+const {
+    [REQUIRED_ENVS.templateRepoOwner]: templateRepoOwner,
+    [REQUIRED_ENVS.templateRepoName]: templateRepoName,
+} = process.env
 
 const router = Router()
 
 router.post(REPOS_GITHUB_PATH + SECRETS_PATH, async (req, res) => {
     const { username, repoName, envName, secretValue } = req.body
-    const getPublicKeyResponse = await githubAPIClient.getRepoPublicEncryptionKey(username, repoName)
+    const getPublicKeyResponse =
+        await githubAPIClient.getRepoPublicEncryptionKey(username, repoName)
     const { key_id: keyId, key } = getPublicKeyResponse.data
     const encryptedSecret = await encryptSecret(key, secretValue)
-    await githubAPIClient.createRepoSecret(username, repoName, envName, encryptedSecret, keyId)
+    await githubAPIClient.createRepoSecret(
+        username,
+        repoName,
+        envName,
+        encryptedSecret,
+        keyId
+    )
     res.json('OK.')
 })
 
 router.post(REPOS_GITHUB_PATH, async (req, res) => {
     const { repoName } = req.body
-    const createRepoResponse = await githubAPIClient.createRepo(repoName)
+    const createRepoResponse = await githubAPIClient.createRepoFromTemplate(
+        repoName,
+        templateRepoName as string,
+        templateRepoOwner as string
+    )
     res.json(createRepoResponse.data)
 })
 
 router.delete(REPOS_GITHUB_PATH, async (req, res) => {
     const { username, repoName } = req.body
-    const deleteRepoResponse = await githubAPIClient.deleteUserRepo(username, repoName)
+    const deleteRepoResponse = await githubAPIClient.deleteUserRepo(
+        username,
+        repoName
+    )
     res.json(deleteRepoResponse.data)
 })
 
