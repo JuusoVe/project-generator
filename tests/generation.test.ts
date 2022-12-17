4658b93fe366e37586445301eae1258153489327
Juuso <juuso.vesanto@gmail.com>
2022-12-17 17:36:17 +0200

    refactor: Rename and rethink using git.

--- tests/generation.test.ts
+++ tests/generation.test.ts
@@ -3,6 +3,7 @@ import { REQUIRED_ENVS } from '../src'
 import app from '../src/app'
 import { FRONTENDS_VERCEL_PROJECT_PATH } from '../src/routes/frontends'
 import { REPOS_GITHUB_PATH, SECRETS_PATH } from '../src/routes/repos'
+import gitClient from '../src/apis/git'
 
 const targetRepoOwner = process.env[REQUIRED_ENVS.targetRepoOwner]
 const TARGET_REPO_NAME = 'generator-test-target-repo'
@@ -11,51 +12,57 @@ const TARGET_REPO_SECRET_VALUE = 'TEST_SECRET_VALUE'
 const TARGET_FRONTEND_PROJECT_NAME = 'test-frontend-project'
 
 // try catches allow us to clean up other resources even if something fails
-afterAll(async () => {
-    const errors: unknown[] = []
-    try {
-        const deleteRepoResponse = await request(app)
-            .delete(REPOS_GITHUB_PATH)
-            .send({ username: targetRepoOwner, repoName: TARGET_REPO_NAME })
-        expect(deleteRepoResponse.statusCode).toBe(200)
-    } catch (err) {
-        errors.push(err)
-    }
-    try {
-        const deleteFrontendProjectResponse = await request(app)
-            .delete(FRONTENDS_VERCEL_PROJECT_PATH)
-            .send({ projectName: TARGET_FRONTEND_PROJECT_NAME })
-        expect(deleteFrontendProjectResponse.statusCode).toBe(200)
-    } catch (err) {
-        errors.push(err)
-    }
-    if (errors.length) {
-        throw errors[0]
-    }
+// afterAll(async () => {
+//     const errors: unknown[] = []
+//     try {
+//         const deleteRepoResponse = await request(app)
+//             .delete(REPOS_GITHUB_PATH)
+//             .send({ username: targetRepoOwner, repoName: TARGET_REPO_NAME })
+//         expect(deleteRepoResponse.statusCode).toBe(200)
+//     } catch (err) {
+//         errors.push(err)
+//     }
+//     try {
+//         const deleteFrontendProjectResponse = await request(app)
+//             .delete(FRONTENDS_VERCEL_PROJECT_PATH)
+//             .send({ projectName: TARGET_FRONTEND_PROJECT_NAME })
+//         expect(deleteFrontendProjectResponse.statusCode).toBe(200)
+//     } catch (err) {
+//         errors.push(err)
+//     }
+//     if (errors.length) {
+//         throw errors[0]
+//     }
+// })
+
+// test('can create a repo', async () => {
+//     const createRepoResponse = await request(app)
+//         .post(REPOS_GITHUB_PATH)
+//         .send({ repoName: TARGET_REPO_NAME })
+//     expect(createRepoResponse.statusCode).toBe(200)
+// })
+
+test('can push code to repo', async () => {
+    await gitClient.pushTemplateRepo()
 })
 
-test('can create a repo', async () => {
-    const createRepoResponse = await request(app)
-        .post(REPOS_GITHUB_PATH)
-        .send({ repoName: TARGET_REPO_NAME })
-    expect(createRepoResponse.statusCode).toBe(200)
-})
+// test('can create repo secrets', async () => {
+//     const createSecretResponse = await request(app)
+//         .post(REPOS_GITHUB_PATH + SECRETS_PATH)
+//         .send({
+//             username: targetRepoOwner,
+//             repoName: TARGET_REPO_NAME,
+//             envName: TARGET_REPO_SECRET_ENV_NAME,
+//             secretValue: TARGET_REPO_SECRET_VALUE,
+//         })
+//     expect(createSecretResponse.statusCode).toBe(200)
+// })
 
-test('can create repo secrets', async () => {
-    const createSecretResponse = await request(app)
-        .post(REPOS_GITHUB_PATH + SECRETS_PATH)
-        .send({
-            username: targetRepoOwner,
-            repoName: TARGET_REPO_NAME,
-            envName: TARGET_REPO_SECRET_ENV_NAME,
-            secretValue: TARGET_REPO_SECRET_VALUE,
-        })
-    expect(createSecretResponse.statusCode).toBe(200)
-})
+// test('can create vercel project', async () => {
+//     const createFrontendProjectResponse = await request(app)
+//         .post(FRONTENDS_VERCEL_PROJECT_PATH)
+//         .send({ projectName: TARGET_FRONTEND_PROJECT_NAME })
+//     expect(createFrontendProjectResponse.statusCode).toBe(200)
+// })
 
-test('can create vercel project', async () => {
-    const createFrontendProjectResponse = await request(app)
-        .post(FRONTENDS_VERCEL_PROJECT_PATH)
-        .send({ projectName: TARGET_FRONTEND_PROJECT_NAME })
-    expect(createFrontendProjectResponse.statusCode).toBe(200)
-})
+// test('can deploy to vercel from repo', async () => {})
